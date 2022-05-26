76c0b55281429e2b6943f29684b690c09db00415
mcs <mcsaygili@outlook.com>
2022-05-26 09:30:56 +0200

    01 directory added

---
+++ 01/vars.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  default = "type here your access key"
+}
+variable "secret_key" {
+  default = "type here your secret key"
+}
+
+variable "aws_key_pair" {
+  default = "type here your secret key location"
+}
